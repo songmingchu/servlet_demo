0ad2f4680a24aef0936d01518c3dfb04d28b732f
DESKTOP-R4EPOBQ\song <songmingchu@163.com>
2017-08-18 12:37:11 +0800

    向页面输出内容

--- src/servlet/ServletDemo.java
+++ src/servlet/ServletDemo.java
@@ -1,10 +1,11 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,8 +27,9 @@ public class ServletDemo extends HttpServlet{
 	}
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("get请求");
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		System.out.println("service_HttpServletRequest");
+		super.service(arg0, arg1);
 	}
 
 	@Override
@@ -37,16 +39,38 @@ public class ServletDemo extends HttpServlet{
 	}
 
 	@Override
-	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.service(arg0, arg1);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//streamWrite(response);
+		
+		printWrite(response);
 	}
 
-	@Override
-	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.service(arg0, arg1);
+	//使用printWriter流输出
+	private void printWrite(HttpServletResponse response) throws IOException {
+		//通过设置响应头，控制浏览器的编码方式
+		response.setHeader("content-type", "text/html; charset=UTF-8");
+		
+		String data = "明初，加油吧！";
+	/*	在获取PrintWriter输出流之前首先使用"response.setCharacterEncoding(charset)"
+		设置字符以什么样的编码输出到浏览器，如：response.setCharacterEncoding("UTF-8");
+		设置将字符以"UTF-8"编码输出到客户端浏览器，然后再使用response.getWriter();
+		获取PrintWriter输出流，这两个步骤不能颠倒 		*/
+		response.setCharacterEncoding("UTF-8");	//设置字符以utf-8的编码输出到浏览器
+		PrintWriter writer = response.getWriter();
+		writer.write(data);
+	}
+
+	//使用OutputStream流输出
+	private void streamWrite(HttpServletResponse response) throws IOException, UnsupportedEncodingException {
+		//通过设置响应头，控制浏览器的编码方式
+		response.setHeader("content-type", "text/html; charset=UTF-8");
+		
+		String a = "自律给人自由";
+		OutputStream out = response.getOutputStream();
+		out.write(a.getBytes("UTF-8")); //使用outputStream流向浏览器输出字节数组
 	}
 
 
+	
+	
 }
